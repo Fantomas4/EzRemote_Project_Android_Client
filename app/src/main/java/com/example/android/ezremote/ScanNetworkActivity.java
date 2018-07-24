bbce03be6c6ace9ce5a8574cfcfd11e6d90f2855
Fantomas4 <stef1997s@gmail.com>
2018-07-24 05:59:47 +0300

    Continued work. Currently working on finishing the implementation of the Refresh Countdown TextView.

--- app/src/main/java/com/example/android/ezremote/ScanNetworkActivity.java
+++ app/src/main/java/com/example/android/ezremote/ScanNetworkActivity.java
@@ -1,19 +1,28 @@
 package com.example.android.ezremote;
 
+import android.os.AsyncTask;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class ScanNetworkActivity extends AppCompatActivity {
 
     private List<DetectedDevice> deviceList = new ArrayList<>();
     private RecyclerView devicesRecyclerView;
     private DevicesRecyclerViewAdapter adapter;
+    private TextView refreshCountdownTextView;
+    private RefreshCountdownHandler refreshCountdownHandler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,8 @@ public class ScanNetworkActivity extends AppCompatActivity {
 
         devicesRecyclerView = (RecyclerView) findViewById(R.id.detected_device_list);
 
+        refreshCountdownTextView = (TextView) findViewById(R.id.refresh_countdown_text_view);
+
         adapter = new DevicesRecyclerViewAdapter(deviceList);
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
@@ -29,13 +40,77 @@ public class ScanNetworkActivity extends AppCompatActivity {
         devicesRecyclerView.setItemAnimator(new DefaultItemAnimator());
         devicesRecyclerView.setAdapter(adapter);
 
+        refreshCountdownHandler = new RefreshCountdownHandler();
+
         prepareDevicesData();
+        new RefreshCountdownTask();
     }
 
+    private class RefreshCountdownHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            refreshCountdownTextView.setText("done!");
+        }
+
+    }
+
+    // temporary method used for creating dummy data to test DevicesRecyclerView
     private void prepareDevicesData() {
         deviceList.add(new DetectedDevice("Stefanos", "102.168.1.2", "Ready"));
         deviceList.add(new DetectedDevice("Panos", "102.168.1.3", "Not Ready"));
 
         adapter.notifyDataSetChanged();
     }
+
+//    private class RefreshCountdownTimer extends CountDownTimer {
+//
+//        public RefreshCountdownTimer(long millisInFuture, long countDownInterval) {
+//            super(millisInFuture, countDownInterval);
+//        }
+//
+//        @Override
+//        public void onTick(long millisUntilFinished) {
+//            long ms = millisUntilFinished;
+//            String text = String.format("%02d\' %02d\"",
+//                    TimeUnit.MILLISECONDS.toMinutes(ms) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(ms)),
+//                    TimeUnit.MILLISECONDS.toSeconds(ms) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(ms)));
+//            refreshCountdownHandler.sendEmptyMessage(0);
+//        }
+//
+//        @Override
+//        public void onFinish() {
+//
+//        }
+//    }
+
+    private class RefreshCountdownTask {
+        Thread refreshCountdownThread;
+        Runnable r;
+
+        public RefreshCountdownTask() {
+            r = new Runnable() {
+                @Override
+                public void run() {
+//                    RefreshCountdownTimer timer = new RefreshCountdownTimer(3000,1000);
+//                    timer.start();
+                    refreshCountdownHandler.sendEmptyMessage(0);
+                }
+            };
+
+            initializeThread();
+        }
+
+        private void initializeThread() {
+            refreshCountdownThread = new Thread(r);
+            refreshCountdownThread.start();
+        }
+
+
+
+
+
+
+
+    }
 }
+
