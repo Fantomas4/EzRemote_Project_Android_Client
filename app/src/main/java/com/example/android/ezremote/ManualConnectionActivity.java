f010cb5d496bcd4e904ac908e915fc8442fec393
Fantomas4 <stefanokf@auth.gr>
2018-09-05 07:15:44 +0300

    Made changes to ManualConnectionActivity's GUI.

--- app/src/main/java/com/example/android/ezremote/ManualConnectionActivity.java
+++ app/src/main/java/com/example/android/ezremote/ManualConnectionActivity.java
@@ -3,14 +3,12 @@ package com.example.android.ezremote;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.JsonReader;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
@@ -19,7 +17,7 @@ import java.util.Map;
 public class ManualConnectionActivity extends AppCompatActivity {
 
 
-    EditText ipInput;
+    EditText ipInputField;
     EditText portInput;
     TextView notificationMsg;
     Button connectButton;
@@ -63,10 +61,9 @@ public class ManualConnectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_connection);
-
-        ipInput = (EditText)findViewById(R.id.ip_input);
-        portInput = (EditText)findViewById(R.id.port_input);
-        notificationMsg = (TextView)findViewById(R.id.notification_msg_textView);
+        ipInputField = (EditText)findViewById(R.id.ipEditText);
+        portInput = (EditText)findViewById(R.id.portEditText);
+        notificationMsg = (TextView)findViewById(R.id.notificationMsgTextView);
         connectButton = (Button)findViewById(R.id.connect_button);
         connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
